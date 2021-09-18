9eb210095531191239cc7a3b4bceb3051186c490
Blogify2021 <Blogify2021@gmail.com>
2021-09-18 15:52:17 +0530

    prototype create,.env create,.gitgnore create

---
+++ send_mail.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+import smtplib
+load_dotenv(".env")
+SENDER_EMAIL_ID = os.getenv("SENDER_EMAIL")
+PASSWORD = os.getenv("BLOGIFY_EMAIL")
+RECIVER_EMAIL_ID = os.getenv("SENDER_PASSWORD")
+
+
+class SendMail:
+    def __init__(self, data: dict) -> None:
+        message = ""
+        for key in data:
+            message += f"{key}:{data[key]}\n"
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=SENDER_EMAIL_ID, password=PASSWORD)
+            connection.sendmail(
+                from_addr=SENDER_EMAIL_ID, to_addrs=RECIVER_EMAIL_ID, msg=f"subject:USER QUERY\n\n{message}")
