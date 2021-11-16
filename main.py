ff343fe1ec4d088ac7bcaa75cea875fa570b8b02
Blogify2021 <Blogify2021@gmail.com>
2021-11-16 09:15:57 +0530

    bug fix

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ def load_user(user_id):
 @app.route('/')
 def get_all_posts():
     posts = BlogPost.query.all()[::-1]
-    return render_template("index.html", all_posts=posts, logged_in=current_user.is_authenticated, current_user=current_user)
+    return render_template("login.html", all_posts=posts, logged_in=current_user.is_authenticated, current_user=current_user)
 
 
 @app.route('/register', methods=["GET", "POST"])
